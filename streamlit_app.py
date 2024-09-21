713f14fdea039d79b7e688b41859f3f10a8ba72b
wildchikorita23 <wildchikorita23@gmail.com>
2024-09-21 14:47:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -556,10 +556,9 @@ evaluation_criteria에 있는 텍스트를 평가 결과를 작성할 때 인용
 
               # QR 코드 이미지 링크
               qr_code_link = "https://i.postimg.cc/gJkt10sM/yuriringqr.png"
-              # QR 코드 이미지 표시
                   st.markdown(
                       f"""
-                      <div style="text-align:center;">
+                      <div style="text-align:center; margin-top:20px;">
                           <img src="{qr_code_link}" alt="Donate QR Code" style="width:150px;"/>
                           <p>여러분의 기부가 이 서비스를 유지하고 발전시키는 데 큰 도움이 됩니다♡</p>
                       </div>
