1840f531c6a4ee0a0c5cbeac0269ce1d322d93ed
wildchikorita23 <wildchikorita23@gmail.com>
2024-09-21 14:39:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,18 +174,7 @@ if password == adminpassword:
 else:
     st.sidebar.warning("올바른 비밀번호를 입력하세요.")
 
-# QR 코드 이미지 링크
-qr_code_link = "https://i.postimg.cc/gJkt10sM/yuriringqr.png"
-# QR 코드 이미지 표시
-st.markdown(
-    f"""
-    <div style="text-align:center;">
-        <img src="{qr_code_link}" alt="Donate QR Code" style="width:150px;"/>
-        <p>여러분의 기부가 이 서비스를 유지하고 발전시키는 데 큰 도움이 됩니다.</p>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+
 
 # 각 평가 항목 초기화
 categories = [
@@ -563,7 +552,7 @@ evaluation_criteria에 있는 텍스트를 평가 결과를 작성할 때 인용
                         '점수': scores
                     })
                 
-
+               
 
                 # 새 평가 데이터 저장
                     feedback_data.append({
@@ -578,6 +567,19 @@ evaluation_criteria에 있는 텍스트를 평가 결과를 작성할 때 인용
                # 최신 예외 처리 방식 적용
                 except Exception as e:
                     st.error(f"예기치 않은 오류가 발생했습니다: {e}")
+
+              # QR 코드 이미지 링크
+               qr_code_link = "https://i.postimg.cc/gJkt10sM/yuriringqr.png"
+               # QR 코드 이미지 표시
+               st.markdown(
+                   f"""
+                   <div style="text-align:center;">
+                       <img src="{qr_code_link}" alt="Donate QR Code" style="width:150px;"/>
+                       <p>여러분의 기부가 이 서비스를 유지하고 발전시키는 데 큰 도움이 됩니다♡</p>
+                   </div>
+                   """,
+                   unsafe_allow_html=True
+               )
 
     except Exception as e:
         st.error(f"PDF 파일을 처리하는 중 오류가 발생했습니다: {e}")
